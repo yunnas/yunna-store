003872c54899e81d8182b998813166e9aae6778f
YuYa <62820666+jianzhis@users.noreply.github.com>
2024-03-12 00:03:41 +0800

    fix(videos): 原视频地址为4k视频导致一直加载视频消耗流量，删除视频地址 - course-details.vue - my-course.vue

--- src/pages/apps/academy/course-details.vue
+++ src/pages/apps/academy/course-details.vue
@@ -52,7 +52,7 @@ const panelStatus = ref(0)
           >
             <div class="px-2 pt-2">
               <VideoPlayer
-                src="https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-576p.mp4"
+                src="https://cdn.plyr.io/"
                 poster="https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-HD.jpg"
                 controls
                 plays-inline
