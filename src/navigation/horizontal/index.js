45bdb6cea6123eac88d0aa8d3f34b4dcf5d65f06
YuYa <yu934075733@gmail.com>
2024-03-13 23:50:39 +0800

    feat(导航栏): 导航栏修改 - 只保留了dashboard和document两个导航栏 - 导航栏中文翻译内容修改

--- src/navigation/horizontal/index.js
+++ src/navigation/horizontal/index.js
@@ -1,10 +1,13 @@
-import apps from './apps'
-import charts from './charts'
+// import apps from './apps'
+// import charts from './charts'
+// import dashboard from './dashboard'
+// import forms from './forms'
+// import misc from './misc'
+// import pages from './pages'
+// import tables from './tables'
+// import uiElements from './ui-elements'
 import dashboard from './dashboard'
-import forms from './forms'
-import misc from './misc'
-import pages from './pages'
-import tables from './tables'
-import uiElements from './ui-elements'
+import document from './document'
 
-export default [...dashboard, ...apps, ...pages, ...uiElements, ...forms, ...tables, ...charts, ...misc]
+// export default [...dashboard, ...apps, ...pages, ...uiElements, ...forms, ...tables, ...charts, ...misc]
+export default [...dashboard, ...document]
